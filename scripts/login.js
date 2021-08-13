f586a217e666d7aa8cf4562ee9512a79c61d04ad
EHashemi <EHashemi.Ana@Gmail.com>
2021-08-13 12:23:35 +0430

    set the background for my login-page

---
+++ scripts/login.js
@@ -0,0 +1,23 @@
+var log=document.getElementById('loginId');
+
+log.addEventListener("click",checkfunc);
+
+function checkfunc(){
+     if (
+        userInput.value.toLowerCase() == "admin@ojvar.dev" &&
+        passInput.value == "@ojvar"
+) {
+    confirm("Welcome");
+} else {
+    alert("Invalid Email/password");
+}
+
+}
+
+
+      var myTimer = setInterval(myFunction, 1000);
+
+      function myFunction() {
+        var d = new Date();
+        document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+      }
