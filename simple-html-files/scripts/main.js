aac6c8f7c31c6ab8dc191b60dc57934e55ef42e3
EHashemi <EHashemi.Ana@Gmail.com>
2021-08-24 00:46:07 +0430

    adding some js events

---
+++ simple-html-files/scripts/main.js
@@ -0,0 +1,6 @@
+var x = document.getElementsByClassName("nav-link")[0];
+x.addEventListener("onmouseover", onMOver);
+
+function onMOver() {
+    document.getElementsByClassName("nav-link")[0].style.color = "white";
+}
