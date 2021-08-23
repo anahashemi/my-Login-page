8f3a6a4a62254df8407eb80b5711fb08f386c875
EHashemi <EHashemi.Ana@Gmail.com>
2021-08-24 02:24:52 +0430

    reforming header buttons

--- simple-html-files/scripts/main.js
+++ simple-html-files/scripts/main.js
@@ -1,6 +1,1 @@
-var x = document.getElementsByClassName("nav-link")[0];
-x.addEventListener("onmouseover", onMOver);
 
-function onMOver() {
-    document.getElementsByClassName("nav-link")[0].style.color = "white";
-}
